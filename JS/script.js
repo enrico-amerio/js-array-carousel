e8c81735ebd59c0c2fd8a68160f912b0691e0337
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-26 14:16:21 +0100

    [add] Array with all imgs

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+const btnUp = document.getElementById('btn-up');
+const btnDown = document.getElementById('btn-down');
+
+const images = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp']
+console.log(images);
+
