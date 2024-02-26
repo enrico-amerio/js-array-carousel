8071b3b9b4d7d42b1bee85940a32558af9b3cc73
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-26 16:56:26 +0100

    [add] preview pictures

--- JS/script.js
+++ JS/script.js
@@ -3,34 +3,44 @@ const btnDown = document.getElementById('btn-down');
 const containerImg = document.querySelector('.container-img')
 const images = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp']
 let counterImg = 0;
-console.log(images);
+const containerPreview = document.querySelector('.container-preview')
 
 for (i = 0; i < images.length; i++){
   const img = images[i];
   console.log(img);
   containerImg.innerHTML += `<img class="img d-none" src="${img}">`
+  containerPreview.innerHTML += `<img class="preview dark" src="${img}">`
 }
 
 const itemCollection = document.getElementsByClassName('img')
 itemCollection[counterImg].classList.remove('d-none')
+const previewCollection = document.getElementsByClassName('preview')
+previewCollection[counterImg].classList.remove('dark')
 
 
 btnUp.addEventListener('click', function(){
   itemCollection[counterImg].classList.add('d-none')
+  previewCollection[counterImg].classList.add('dark')
   counterImg--
   if (counterImg < 0) {
     counterImg = images.length - 1;
   } 
 
     itemCollection[counterImg].classList.remove('d-none')
+    previewCollection[counterImg].classList.remove('dark')
+
   
 } )
 btnDown.addEventListener('click', function(){
   
   itemCollection[counterImg].classList.add('d-none')
+  previewCollection[counterImg].classList.add('dark')
+
   counterImg++
   if (counterImg > 4) {
     counterImg = 0;
   } 
   itemCollection[counterImg].classList.remove('d-none')
+  previewCollection[counterImg].classList.remove('dark')
+
 } )
