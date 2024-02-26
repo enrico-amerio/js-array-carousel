223914e680502d67a68ae6a099134ba4e7d27e37
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-26 15:16:24 +0100

    [add] Infinite cycle to JS

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ const btnUp = document.getElementById('btn-up');
 const btnDown = document.getElementById('btn-down');
 const containerImg = document.querySelector('.container-img')
 const images = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp']
-let counterImg = 1;
+let counterImg = 0;
 console.log(images);
 
 for (i = 0; i < images.length; i++){
@@ -18,13 +18,19 @@ itemCollection[counterImg].classList.remove('d-none')
 btnUp.addEventListener('click', function(){
   itemCollection[counterImg].classList.add('d-none')
   counterImg--
-  itemCollection[counterImg].classList.remove('d-none')
-  console.log(counterImg);
+  if (counterImg < 0) {
+    counterImg = images.length - 1;
+  } 
+
+    itemCollection[counterImg].classList.remove('d-none')
+  
 } )
 btnDown.addEventListener('click', function(){
   
   itemCollection[counterImg].classList.add('d-none')
   counterImg++
+  if (counterImg > 4) {
+    counterImg = 0;
+  } 
   itemCollection[counterImg].classList.remove('d-none')
-  console.log(counterImg);
 } )
